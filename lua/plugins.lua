e528714fde56570a6100b37145eff617a2191aaa
Louis Montaut <louismontaut@gmail.com>
2023-04-11 17:38:41 +0200

    Adding "tpope/vim-vinegar" plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -64,6 +64,9 @@ require('packer').startup(function(use)
   use { 'tpope/vim-rhubarb' }
   use { 'lewis6991/gitsigns.nvim' }
 
+  -- Better netrw
+  use { 'tpope/vim-vinegar' }
+
   use { 'navarasu/onedark.nvim' } -- Theme inspired by Atom
   use { 'nvim-lualine/lualine.nvim' } -- Fancier statusline
   use { 'lukas-reineke/indent-blankline.nvim' } -- Add indentation guides even on blank lines
