6c224c1fdb7a8a8e0696e306b2bfb1cb932d2e6c
Louis Montaut <louismontaut@gmail.com>
2023-04-27 17:49:36 +0200

    Adding nvim cmds for Pap horizontal/vertical modes

--- lua/plugins_config.lua
+++ lua/plugins_config.lua
@@ -865,16 +865,22 @@ vim.keymap.set('n', "<leader>u", vim.cmd.UndotreeToggle, { desc = "Undo history"
 -------------------------
 local pap = require("pap")
 vim.api.nvim_create_user_command('Papcmd', function(command) pap.set_cmd(command.args) end, { nargs = "*" })
-vim.api.nvim_create_user_command('Pap', function(command) pap.run_cmd(command.args, false) end, { nargs = "*" })
-vim.api.nvim_create_user_command('Papsize', function(command) pap.set_window_size(command.args) end, { nargs = "*" })
+vim.api.nvim_create_user_command('Pap', function(command) pap.run_cmd(command.args) end, { nargs = "*" })
+vim.api.nvim_create_user_command('Paphsize', function(command) pap.set_window_hsize(command.args) end, { nargs = "*" })
+vim.api.nvim_create_user_command('Papvsize', function(command) pap.set_window_vsize(command.args) end, { nargs = "*" })
+vim.api.nvim_create_user_command('PapSetHorizontal', function() pap.set_horizontal_window() end, {})
+vim.api.nvim_create_user_command('PapSetVertical', function() pap.set_vertical_window() end, {})
 vim.api.nvim_create_user_command('Paprun', function(command) pap.run_custom_cmd(command.args, false) end, { nargs = "*" })
 vim.api.nvim_create_user_command('Par', function(command) pap.run_custom_cmd(command.args, false) end, { nargs = "*" })
 vim.api.nvim_create_user_command('Papa', function(command) pap.run_custom_cmd(command.args, false) end, { nargs = "*" })
 vim.keymap.set("n", "<leader>qe", ":cg<Space>~/.cache/nvim/out.txt<CR>:cc<CR>", { noremap = true, silent = true, desc = "Load error file into quickfix" })
-vim.keymap.set("n", "<leader>ps", ":Papsize<CR>", { noremap = true, silent = true, desc = "Set pap window size" })
+vim.keymap.set("n", "<leader>psv", ":Paphsize<CR>", { noremap = true, silent = true, desc = "Set pap horizontal window size" })
+vim.keymap.set("n", "<leader>psh", ":Papvsize<CR>", { noremap = true, silent = true, desc = "Set pap vertical window size" })
 vim.keymap.set("n", "<leader>pr", ":Paprun<CR>",  { noremap = true, silent = true, desc = "Paprun (any cmd)" })
 vim.keymap.set("n", "<leader>pc", ":Papcmd<CR>",  { noremap = true, silent = true, desc = "Set pap default cmd" })
 vim.keymap.set("n", "<leader>pp", ":Pap<space>",  { noremap = true, silent = false, desc = "Pap (prefix default cmd)" })
+vim.keymap.set("n", "<leader>pv", ":PapSetVertical<CR>",  { noremap = true, silent = true, desc = "Pap set vertical mode" })
+vim.keymap.set("n", "<leader>ph", ":PapSetHorizontal<CR>",  { noremap = true, silent = true, desc = "Pap set horizontal mode" })
 
 -------------------------
 -- [[ Configure DAP ]] --
