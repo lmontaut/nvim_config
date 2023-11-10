d1f27a9e4519c2736df8706c9ce6376603d866a0
Louis Montaut <louismontaut@gmail.com>
2023-11-10 16:01:43 +0100

    Removing useless plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -150,7 +150,7 @@ require('packer').startup(function(use)
   -- use { "epwalsh/obsidian.nvim" }
 
   -- Mardown preview
-  use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+  -- use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 
   -- C# dev
   -- use { 'OmniSharp/omnisharp-vim' }
