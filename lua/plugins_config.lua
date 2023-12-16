37f1aefe101a481df1a88524df276b25b1658971
Louis Montaut <louismontaut@gmail.com>
2023-12-16 14:35:56 +0100

    Better root project management

--- lua/plugins_config.lua
+++ lua/plugins_config.lua
@@ -596,7 +596,7 @@ if has_lspconfig and false then
     on_attach = on_attach,
     capabilities = capabilities,
     filetypes = { "c", "cpp" },
-    root_dir = util.root_pattern('compile_commands.json', '.git', '.ccls'),
+    root_dir = util.root_pattern('compile_commands.json', '.git'),
     init_options = {
       index = {
         threads = 0;
@@ -692,7 +692,7 @@ if has_project then
   project.setup({
     -- Manual mode doesn't automatically change your root directory, so you have
     -- the option to manually do so using `:ProjectRoot` command.
-    manual_mode = false,
+    manual_mode = true,
     -- Methods of detecting the root directory. **"lsp"** uses the native neovim
     -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
     -- order matters: if one is not detected, the other is used as fallback. You
@@ -702,7 +702,7 @@ if has_project then
     -- All the patterns used to detect root dir, when **"pattern"** is in
     -- detection_methods
     -- patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
-    patterns = { ".git", "package.json" },
+    patterns = { ".git", ".clangd" },
     -- Table of lsp clients to ignore by name
     -- eg: { "efm", ... }
     -- ignore_lsp = {},
@@ -721,6 +721,7 @@ if has_project then
     require('telescope').load_extension('projects')
     vim.keymap.set('n', '<leader>sp', "<CMD>Telescope projects<CR>", { desc = 'Open project...' })
   end
+  vim.keymap.set('n', '<leader>0', "<CMD>ProjectRoot<CR>", { desc = 'Set project root to current project' })
 end
 
 --------------------------------
