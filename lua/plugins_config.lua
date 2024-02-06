3cb439200b1369e86a57fd8fd8769e631fc6821e
Louis Montaut <louismontaut@gmail.com>
2024-02-06 16:02:53 +0100

    Project: automatically switch dir

--- lua/plugins_config.lua
+++ lua/plugins_config.lua
@@ -696,7 +696,7 @@ if has_project then
   project.setup({
     -- Manual mode doesn't automatically change your root directory, so you have
     -- the option to manually do so using `:ProjectRoot` command.
-    manual_mode = true,
+    manual_mode = false,
     -- Methods of detecting the root directory. **"lsp"** uses the native neovim
     -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
     -- order matters: if one is not detected, the other is used as fallback. You
