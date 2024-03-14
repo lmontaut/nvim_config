f0413f21a61503aec33c9c3c1cfa073242537dd4
Louis Montaut <louismontaut@gmail.com>
2024-03-14 16:54:32 +0100

    Harpoon: use telescope for quick menu

--- after/plugin/plugins_config.lua
+++ after/plugin/plugins_config.lua
@@ -2343,11 +2343,32 @@ end
 -----------------------------
 -- [[ Configure harpoon ]] --
 -----------------------------
-
 local has_harpoon, harpoon = pcall(require, "harpoon")
 if has_harpoon then
+  harpoon:setup()
+  -- basic telescope configuration
+  local conf = require("telescope.config").values
+  local function toggle_telescope(harpoon_files)
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+      table.insert(file_paths, item.value)
+    end
+
+    require("telescope.pickers").new({}, {
+      prompt_title = "Harpoon",
+      finder = require("telescope.finders").new_table({
+        results = file_paths,
+      }),
+      previewer = conf.file_previewer({}),
+      sorter = conf.generic_sorter({}),
+    }):find()
+  end
+
   vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end, { desc = "Harpoon: append to list" })
-  vim.keymap.set("n", "<leader>.", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+  vim.keymap.set("n", "<leader>.", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
+  -- vim.keymap.set("n", "<leader>.", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+end
+
 end
 
 -------------------------------
